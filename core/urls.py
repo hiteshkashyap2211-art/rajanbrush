144668859f5819db0d9c84ecbb8b20caefb48806
hiteshkashyap2211-art <hiteshkashyap2211@gmail.com>
2026-09-07 22:48:42 +0530

    Fix media files serving for production without breaking routes

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from catalog import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("contact/", views.contact_view, name="contact"),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
